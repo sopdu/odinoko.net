4dde8f81bfd6a3abbc0eb84181812c379144d8b9
sopdu <admin@sopdu.ru>
2019-01-01 19:51:09 +0300

    l

--- sopdu/modules/user/location.php
+++ sopdu/modules/user/location.php
@@ -1,23 +1,26 @@
 <?php
 class userLocation {
-    function getIP(){
-        return $_SERVER["REMOTE_ADDR"];
-    }
     function getCity(){
-
-        return;
-    }
-    function addCity(){
-
-        return;
-    }
-    function editCity($idCity){
-
-        return;
+        $ch = curl_init('http://ipgeobase.ru:7020/geo?ip=' .$_SERVER["REMOTE_ADDR"]);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        $string = curl_exec($ch);
+        # $encode = mb_detect_encoding($string);
+        $sit = iconv('windows-1251', 'UTF-8', $string);
+        $params = array('inetnum', 'country', 'city', 'region', 'district', 'lat', 'lng');
+        $data = $out = array();
+        foreach ($params as $param) {
+            if (preg_match('#<' . $param . '>(.*)</' . $param . '>#is', $sit, $out)) {
+                $data[$param] = trim($out[1]);
+            }
+        }
+        return $data;
     }
     function main(){
-        echo '<pre>'; print_r(self::getIP()); '</pre>';
-        return;
+        $data = self::getCity();
+        return $data;
     }
 }
 ?>
